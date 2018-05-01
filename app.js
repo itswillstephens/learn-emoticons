2a9ec6a15c7c61dc1f4b16ad3372d4b8c17bef6c
Will Stephens <willstephens@Wills-Air.fios-router.home>
2018-05-01 18:13:35 -0400

    Remove non face emojis

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ const emojiFeed = [
     "keywords": "face | face with tears of joy | joy | laugh | tear"
   },
   {
-    "no": 4,
-    "codes": "1F923",
-    "char": "🤣",
-    "name": "rolling on the floor laughing",
-    "keywords": "face | floor | laugh | rolling | rolling on the floor laughing"
-  },
-  {
     "no": 5,
     "codes": "1F603",
     "char": "😃",
@@ -119,48 +112,6 @@ const emojiFeed = [
     "keywords": "closed | eye | face | kiss | kissing face with closed eyes"
   },
   {
-    "no": 19,
-    "codes": "263A",
-    "char": "☺",
-    "name": "smiling face",
-    "keywords": "face | outlined | relaxed | smile | smiling face"
-  },
-  {
-    "no": 20,
-    "codes": "1F642",
-    "char": "🙂",
-    "name": "slightly smiling face",
-    "keywords": "face | slightly smiling face | smile"
-  },
-  {
-    "no": 21,
-    "codes": "1F917",
-    "char": "🤗",
-    "name": "hugging face",
-    "keywords": "face | hug | hugging"
-  },
-  {
-    "no": 22,
-    "codes": "1F929",
-    "char": "🤩",
-    "name": "star-struck",
-    "keywords": "eyes | face | grinning | star | star-struck | starry-eyed"
-  },
-  {
-    "no": 23,
-    "codes": "1F914",
-    "char": "🤔",
-    "name": "thinking face",
-    "keywords": "face | thinking"
-  },
-  {
-    "no": 24,
-    "codes": "1F928",
-    "char": "🤨",
-    "name": "face with raised eyebrow",
-    "keywords": "distrust | face with raised eyebrow | skeptic | disapproval | disbelief | mild surprise | scepticism"
-  },
-  {
     "no": 25,
     "codes": "1F610",
     "char": "😐",
@@ -180,13 +131,6 @@ const emojiFeed = [
     "char": "😶",
     "name": "face without mouth",
     "keywords": "face | face without mouth | mouth | quiet | silent"
-  },
-  {
-    "no": 28,
-    "codes": "1F644",
-    "char": "🙄",
-    "name": "face with rolling eyes",
-    "keywords": "eyes | face | face with rolling eyes | rolling"
   },
   {
     "no": 29,
@@ -215,13 +159,6 @@ const emojiFeed = [
     "char": "😮",
     "name": "face with open mouth",
     "keywords": "face | face with open mouth | mouth | open | sympathy"
-  },
-  {
-    "no": 33,
-    "codes": "1F910",
-    "char": "🤐",
-    "name": "zipper-mouth face",
-    "keywords": "face | mouth | zipper | zipper-mouth face"
   },
   {
     "no": 34,
@@ -280,13 +217,6 @@ const emojiFeed = [
     "keywords": "eye | face | horrible | squinting face with tongue | taste | tongue"
   },
   {
-    "no": 42,
-    "codes": "1F924",
-    "char": "🤤",
-    "name": "drooling face",
-    "keywords": "drooling | face"
-  },
-  {
     "no": 43,
     "codes": "1F612",
     "char": "😒",
@@ -315,39 +245,11 @@ const emojiFeed = [
     "keywords": "confused | face"
   },
   {
-    "no": 47,
-    "codes": "1F643",
-    "char": "🙃",
-    "name": "upside-down face",
-    "keywords": "face | upside-down"
-  },
-  {
-    "no": 48,
-    "codes": "1F911",
-    "char": "🤑",
-    "name": "money-mouth face",
-    "keywords": "face | money | money-mouth face | mouth"
-  },
-  {
     "no": 49,
     "codes": "1F632",
     "char": "😲",
     "name": "astonished face",
     "keywords": "astonished | face | shocked | totally"
-  },
-  {
-    "no": 50,
-    "codes": "2639",
-    "char": "☹",
-    "name": "frowning face",
-    "keywords": "face | frown | frowning face"
-  },
-  {
-    "no": 51,
-    "codes": "1F641",
-    "char": "🙁",
-    "name": "slightly frowning face",
-    "keywords": "face | frown | slightly frowning face"
   },
   {
     "no": 52,
@@ -420,13 +322,6 @@ const emojiFeed = [
     "keywords": "face | tired | weary"
   },
   {
-    "no": 62,
-    "codes": "1F92F",
-    "char": "🤯",
-    "name": "exploding head",
-    "keywords": "exploding head | shocked"
-  },
-  {
     "no": 63,
     "codes": "1F62C",
     "char": "😬",
@@ -455,13 +350,6 @@ const emojiFeed = [
     "keywords": "dazed | face | flushed"
   },
   {
-    "no": 69,
-    "codes": "1F92A",
-    "char": "🤪",
-    "name": "zany face",
-    "keywords": "eye | goofy | large | small | zany face"
-  },
-  {
     "no": 70,
     "codes": "1F635",
     "char": "😵",
@@ -483,13 +371,6 @@ const emojiFeed = [
     "keywords": "angry | face | mad"
   },
   {
-    "no": 73,
-    "codes": "1F92C",
-    "char": "🤬",
-    "name": "face with symbols on mouth",
-    "keywords": "face with symbols on mouth | swearing | cursing"
-  },
-  {
     "no": 74,
     "codes": "1F637",
     "char": "😷",
@@ -497,88 +378,11 @@ const emojiFeed = [
     "keywords": "cold | doctor | face | face with medical mask | mask | medicine | sick"
   },
   {
-    "no": 75,
-    "codes": "1F912",
-    "char": "🤒",
-    "name": "face with thermometer",
-    "keywords": "face | face with thermometer | ill | sick | thermometer"
-  },
-  {
-    "no": 76,
-    "codes": "1F915",
-    "char": "🤕",
-    "name": "face with head-bandage",
-    "keywords": "bandage | face | face with head-bandage | hurt | injury"
-  },
-  {
-    "no": 77,
-    "codes": "1F922",
-    "char": "🤢",
-    "name": "nauseated face",
-    "keywords": "face | nauseated | vomit"
-  },
-  {
-    "no": 78,
-    "codes": "1F92E",
-    "char": "🤮",
-    "name": "face vomiting",
-    "keywords": "face vomiting | sick | vomit"
-  },
-  {
-    "no": 79,
-    "codes": "1F927",
-    "char": "🤧",
-    "name": "sneezing face",
-    "keywords": "face | gesundheit | sneeze | sneezing face"
-  },
-  {
     "no": 80,
     "codes": "1F607",
     "char": "😇",
     "name": "smiling face with halo",
     "keywords": "angel | face | fairy tale | fantasy | halo | innocent | smile | smiling face with halo"
-  },
-  {
-    "no": 81,
-    "codes": "1F920",
-    "char": "🤠",
-    "name": "cowboy hat face",
-    "keywords": "cowboy | cowgirl | face | hat"
-  },
-  {
-    "no": 85,
-    "codes": "1F925",
-    "char": "🤥",
-    "name": "lying face",
-    "keywords": "face | lie | lying face | pinocchio"
-  },
-  {
-    "no": 86,
-    "codes": "1F92B",
-    "char": "🤫",
-    "name": "shushing face",
-    "keywords": "quiet | shush | shushing face"
-  },
-  {
-    "no": 87,
-    "codes": "1F92D",
-    "char": "🤭",
-    "name": "face with hand over mouth",
-    "keywords": "face with hand over mouth | whoops | shock | sudden realization | surprise"
-  },
-  {
-    "no": 88,
-    "codes": "1F9D0",
-    "char": "🧐",
-    "name": "face with monocle",
-    "keywords": "face with monocle | stuffy | wealthy"
-  },
-  {
-    "no": 89,
-    "codes": "1F913",
-    "char": "🤓",
-    "name": "nerd face",
-    "keywords": "face | geek | nerd"
   },
   {
     "no": 90,
@@ -593,13 +397,6 @@ const emojiFeed = [
     "char": "👿",
     "name": "angry face with horns",
     "keywords": "angry face with horns | demon | devil | face | fairy tale | fantasy | imp"
-  },
-  {
-    "no": 92,
-    "codes": "1F921",
-    "char": "🤡",
-    "name": "clown face",
-    "keywords": "clown | face"
   },
   {
     "no": 93,
@@ -642,13 +439,6 @@ const emojiFeed = [
     "char": "👾",
     "name": "alien monster",
     "keywords": "alien | creature | extraterrestrial | face | fairy tale | fantasy | monster | ufo"
-  },
-  {
-    "no": 100,
-    "codes": "1F916",
-    "char": "🤖",
-    "name": "robot face",
-    "keywords": "face | monster | robot"
   },
   {
     "no": 101,
@@ -749,13 +539,6 @@ const emojiFeed = [
     "keywords": "baby | young"
   },
   {
-    "no": 115,
-    "codes": "1F9D2",
-    "char": "🧒",
-    "name": "child",
-    "keywords": "child | gender-neutral | young | (no specific gender)"
-  },
-  {
     "no": 116,
     "codes": "1F466",
     "char": "👦",
@@ -768,13 +551,6 @@ const emojiFeed = [
     "char": "👧",
     "name": "girl",
     "keywords": "girl | Virgo | young | zodiac"
-  },
-  {
-    "no": 118,
-    "codes": "1F9D1",
-    "char": "🧑",
-    "name": "adult",
-    "keywords": "adult | gender-neutral | (no specific gender)"
   },
   {
     "no": 119,
@@ -791,13 +567,6 @@ const emojiFeed = [
     "keywords": "woman"
   },
   {
-    "no": 121,
-    "codes": "1F9D3",
-    "char": "🧓",
-    "name": "older adult",
-    "keywords": "gender-neutral | old | older adult | (no specific gender)"
-  },
-  {
     "no": 122,
     "codes": "1F474",
     "char": "👴",
@@ -812,242 +581,11 @@ const emojiFeed = [
     "keywords": "old | woman"
   },
   {
-    "no": 124,
-    "codes": "1F468 200D 2695 FE0F",
-    "char": "👨‍⚕️",
-    "name": "man health worker",
-    "keywords": "doctor | healthcare | man | man health worker | nurse | therapist"
-  },
-  {
-    "no": 125,
-    "codes": "1F469 200D 2695 FE0F",
-    "char": "👩‍⚕️",
-    "name": "woman health worker",
-    "keywords": "doctor | healthcare | nurse | therapist | woman | woman health worker"
-  },
-  {
-    "no": 126,
-    "codes": "1F468 200D 1F393",
-    "char": "👨‍🎓",
-    "name": "man student",
-    "keywords": "graduate | man | student"
-  },
-  {
-    "no": 127,
-    "codes": "1F469 200D 1F393",
-    "char": "👩‍🎓",
-    "name": "woman student",
-    "keywords": "graduate | student | woman"
-  },
-  {
-    "no": 128,
-    "codes": "1F468 200D 1F3EB",
-    "char": "👨‍🏫",
-    "name": "man teacher",
-    "keywords": "instructor | man | professor | teacher"
-  },
-  {
-    "no": 129,
-    "codes": "1F469 200D 1F3EB",
-    "char": "👩‍🏫",
-    "name": "woman teacher",
-    "keywords": "instructor | professor | teacher | woman"
-  },
-  {
-    "no": 130,
-    "codes": "1F468 200D 2696 FE0F",
-    "char": "👨‍⚖️",
-    "name": "man judge",
-    "keywords": "justice | man | man judge | scales"
-  },
-  {
-    "no": 131,
-    "codes": "1F469 200D 2696 FE0F",
-    "char": "👩‍⚖️",
-    "name": "woman judge",
-    "keywords": "judge | scales | woman"
-  },
-  {
-    "no": 132,
-    "codes": "1F468 200D 1F33E",
-    "char": "👨‍🌾",
-    "name": "man farmer",
-    "keywords": "farmer | gardener | man | rancher"
-  },
-  {
-    "no": 133,
-    "codes": "1F469 200D 1F33E",
-    "char": "👩‍🌾",
-    "name": "woman farmer",
-    "keywords": "farmer | gardener | rancher | woman"
-  },
-  {
-    "no": 134,
-    "codes": "1F468 200D 1F373",
-    "char": "👨‍🍳",
-    "name": "man cook",
-    "keywords": "chef | cook | man"
-  },
-  {
-    "no": 135,
-    "codes": "1F469 200D 1F373",
-    "char": "👩‍🍳",
-    "name": "woman cook",
-    "keywords": "chef | cook | woman"
-  },
-  {
-    "no": 136,
-    "codes": "1F468 200D 1F527",
-    "char": "👨‍🔧",
-    "name": "man mechanic",
-    "keywords": "electrician | man | mechanic | plumber | tradesperson"
-  },
-  {
-    "no": 137,
-    "codes": "1F469 200D 1F527",
-    "char": "👩‍🔧",
-    "name": "woman mechanic",
-    "keywords": "electrician | mechanic | plumber | tradesperson | woman"
-  },
-  {
-    "no": 138,
-    "codes": "1F468 200D 1F3ED",
-    "char": "👨‍🏭",
-    "name": "man factory worker",
-    "keywords": "assembly | factory | industrial | man | worker"
-  },
-  {
-    "no": 139,
-    "codes": "1F469 200D 1F3ED",
-    "char": "👩‍🏭",
-    "name": "woman factory worker",
-    "keywords": "assembly | factory | industrial | woman | worker"
-  },
-  {
-    "no": 140,
-    "codes": "1F468 200D 1F4BC",
-    "char": "👨‍💼",
-    "name": "man office worker",
-    "keywords": "architect | business | man | man office worker | manager | office | white-collar"
-  },
-  {
-    "no": 141,
-    "codes": "1F469 200D 1F4BC",
-    "char": "👩‍💼",
-    "name": "woman office worker",
-    "keywords": "architect | business | manager | office | white-collar | woman | woman office worker"
-  },
-  {
-    "no": 142,
-    "codes": "1F468 200D 1F52C",
-    "char": "👨‍🔬",
-    "name": "man scientist",
-    "keywords": "biologist | chemist | engineer | man | mathematician | physicist | scientist"
-  },
-  {
-    "no": 143,
-    "codes": "1F469 200D 1F52C",
-    "char": "👩‍🔬",
-    "name": "woman scientist",
-    "keywords": "biologist | chemist | engineer | mathematician | physicist | scientist | woman"
-  },
-  {
-    "no": 144,
-    "codes": "1F468 200D 1F4BB",
-    "char": "👨‍💻",
-    "name": "man technologist",
-    "keywords": "coder | developer | inventor | man | software | technologist"
-  },
-  {
-    "no": 145,
-    "codes": "1F469 200D 1F4BB",
-    "char": "👩‍💻",
-    "name": "woman technologist",
-    "keywords": "coder | developer | inventor | software | technologist | woman"
-  },
-  {
-    "no": 146,
-    "codes": "1F468 200D 1F3A4",
-    "char": "👨‍🎤",
-    "name": "man singer",
-    "keywords": "actor | entertainer | man | rock | singer | star"
-  },
-  {
-    "no": 147,
-    "codes": "1F469 200D 1F3A4",
-    "char": "👩‍🎤",
-    "name": "woman singer",
-    "keywords": "actor | entertainer | rock | singer | star | woman"
-  },
-  {
-    "no": 148,
-    "codes": "1F468 200D 1F3A8",
-    "char": "👨‍🎨",
-    "name": "man artist",
-    "keywords": "artist | man | palette"
-  },
-  {
-    "no": 149,
-    "codes": "1F469 200D 1F3A8",
-    "char": "👩‍🎨",
-    "name": "woman artist",
-    "keywords": "artist | palette | woman"
-  },
-  {
-    "no": 150,
-    "codes": "1F468 200D 2708 FE0F",
-    "char": "👨‍✈️",
-    "name": "man pilot",
-    "keywords": "man | pilot | plane"
-  },
-  {
-    "no": 151,
-    "codes": "1F469 200D 2708 FE0F",
-    "char": "👩‍✈️",
-    "name": "woman pilot",
-    "keywords": "pilot | plane | woman"
-  },
-  {
-    "no": 152,
-    "codes": "1F468 200D 1F680",
-    "char": "👨‍🚀",
-    "name": "man astronaut",
-    "keywords": "astronaut | man | rocket"
-  },
-  {
-    "no": 153,
-    "codes": "1F469 200D 1F680",
-    "char": "👩‍🚀",
-    "name": "woman astronaut",
-    "keywords": "astronaut | rocket | woman"
-  },
-  {
-    "no": 154,
-    "codes": "1F468 200D 1F692",
-    "char": "👨‍🚒",
-    "name": "man firefighter",
-    "keywords": "firefighter | firetruck | man"
-  },
-  {
-    "no": 155,
-    "codes": "1F469 200D 1F692",
-    "char": "👩‍🚒",
-    "name": "woman firefighter",
-    "keywords": "firefighter | firetruck | woman"
-  },
-  {
     "no": 156,
     "codes": "1F46E",
     "char": "👮",
     "name": "police officer",
     "keywords": "cop | officer | police"
-  },
-  {
-    "no": 159,
-    "codes": "1F575",
-    "char": "🕵",
-    "name": "detective",
-    "keywords": "detective | sleuth | spy"
   },
   {
     "no": 162,
@@ -1064,13 +602,6 @@ const emojiFeed = [
     "keywords": "construction | hat | worker"
   },
   {
-    "no": 168,
-    "codes": "1F934",
-    "char": "🤴",
-    "name": "prince",
-    "keywords": "prince"
-  },
-  {
     "no": 169,
     "codes": "1F478",
     "char": "👸",
@@ -1085,32 +616,11 @@ const emojiFeed = [
     "keywords": "person wearing turban | turban"
   },
   {
-    "no": 171,
-    "codes": "1F473 200D 2642 FE0F",
-    "char": "👳‍",
-    "name": "wearing turban",
-    "keywords": "man | man wearing turban | turban"
-  },
-  {
     "no": 173,
     "codes": "1F472",
     "char": "👲",
     "name": "man with Chinese cap",
     "keywords": "gua pi mao | hat | man | man with Chinese cap"
-  },
-  {
-    "no": 174,
-    "codes": "1F9D5",
-    "char": "🧕",
-    "name": "woman with headscarf",
-    "keywords": "headscarf | hijab | mantilla | tichel | woman with headscarf | bandana | head kerchief"
-  },
-  {
-    "no": 175,
-    "codes": "1F9D4",
-    "char": "🧔",
-    "name": "bearded person",
-    "keywords": "beard | bearded person | bewhiskered"
   },
   {
     "no": 176,
@@ -1120,32 +630,11 @@ const emojiFeed = [
     "keywords": "blond | blond-haired person"
   },
   {
-    "no": 187,
-    "codes": "1F935",
-    "char": "🤵",
-    "name": "man in tuxedo",
-    "keywords": "groom | man | man in tuxedo | tuxedo"
-  },
-  {
     "no": 188,
     "codes": "1F470",
     "char": "👰",
     "name": "bride with veil",
     "keywords": "bride | bride with veil | veil | wedding"
-  },
-  {
-    "no": 189,
-    "codes": "1F930",
-    "char": "🤰",
-    "name": "pregnant woman",
-    "keywords": "pregnant | woman"
-  },
-  {
-    "no": 190,
-    "codes": "1F931",
-    "char": "🤱",
-    "name": "breast-feeding",
-    "keywords": "baby | breast | breast-feeding | nursing"
   },
   {
     "no": 191,
@@ -1160,83 +649,6 @@ const emojiFeed = [
     "char": "🎅",
     "name": "Santa Claus",
     "keywords": "celebration | Christmas | claus | father | santa | Santa Claus"
-  },
-  {
-    "no": 193,
-    "codes": "1F936",
-    "char": "🤶",
-    "name": "Mrs. Claus",
-    "keywords": "celebration | Christmas | claus | mother | Mrs. | Mrs. Claus"
-  },
-  {
-    "no": 200,
-    "codes": "1F9D9",
-    "char": "🧙",
-    "name": "mage",
-    "keywords": "mage | sorcerer | sorceress | witch | wizard"
-  },
-  {
-    "no": 201,
-    "codes": "1F9D9 200D 2640 FE0F",
-    "char": "🧙‍",
-    "name": "mage",
-    "keywords": "sorceress | witch | woman mage"
-  },
-  {
-    "no": 203,
-    "codes": "1F9DA",
-    "char": "🧚",
-    "name": "fairy",
-    "keywords": "fairy | Oberon | Puck | Titania"
-  },
-  {
-    "no": 206,
-    "codes": "1F9DB",
-    "char": "🧛",
-    "name": "vampire",
-    "keywords": "Dracula | undead | vampire"
-  },
-  {
-    "no": 207,
-    "codes": "1F9DB 200D 2640 FE0F",
-    "char": "🧛‍",
-    "name": "vampire",
-    "keywords": "undead | woman vampire"
-  },
-  {
-    "no": 209,
-    "codes": "1F9DC",
-    "char": "🧜",
-    "name": "merperson",
-    "keywords": "mermaid | merman | merperson | merwoman"
-  },
-  {
-    "no": 210,
-    "codes": "1F9DC 200D 2640 FE0F",
-    "char": "🧜‍",
-    "name": "mermaid",
-    "keywords": "mermaid | merwoman"
-  },
-  {
-    "no": 212,
-    "codes": "1F9DD",
-    "char": "🧝",
-    "name": "elf",
-    "keywords": "elf | magical | LOTR style"
-  },
-  {
-    "no": 215,
-    "codes": "1F9DE",
-    "char": "🧞",
-    "name": "genie",
-    "keywords": "djinn | genie | (non-human color)"
-  },
-  {
-    "no": 218,
-    "codes": "1F9DF",
-    "char": "🧟",
-    "name": "zombie",
-    "keywords": "undead | walking dead | zombie | (non-human color)"
   },
   {
     "no": 221,
@@ -1286,20 +698,6 @@ const emojiFeed = [
     "char": "🙇",
     "name": "person bowing",
     "keywords": "apology | bow | gesture | person bowing | sorry"
-  },
-  {
-    "no": 242,
-    "codes": "1F926",
-    "char": "🤦",
-    "name": "person facepalming",
-    "keywords": "disbelief | exasperation | face | palm | person facepalming"
-  },
-  {
-    "no": 245,
-    "codes": "1F937",
-    "char": "🤷",
-    "name": "person shrugging",
-    "keywords": "doubt | ignorance | indifference | person shrugging | shrug"
   },
   {
     "no": 248,
